3bfe7670c09479822d75f006c13232ff2e0f1255
YuCao16 <yu.carl@outlook.com>
2022-07-30 01:10:42 +0100

    Initial commit

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,20 @@
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+--            ███╗░░██╗███████╗░█████╗░██╗░░░██╗██╗███╗░░░███╗		  --
+--            ████╗░██║██╔════╝██╔══██╗██║░░░██║██║████╗░████║		  --
+--            ██╔██╗██║█████╗░░██║░░██║╚██╗░██╔╝██║██╔████╔██║		  --
+--            ██║╚████║██╔══╝░░██║░░██║░╚████╔╝░██║██║╚██╔╝██║		  --
+--            ██║░╚███║███████╗╚█████╔╝░░╚██╔╝░░██║██║░╚═╝░██║		  --
+--            ╚═╝░░╚══╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝		  --
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+vim.cmd([[let g:python3_host_prog="/Library/Frameworks/Python.framework/Versions/3.9/bin/python3"]])
+
+require("impatient") --speed up start up
+require("plugins") -- all plugins and some configs using packer.nvim
+require("bubbles") -- custom lualine
+require("mappings") -- custom mappings
+vim.cmd("source $HOME/.config/nvim/settings.vim")
+require("vimplug_settings") -- vim plugins settings
+require("general_settings") -- neovim general settings
+require("colorscheme") -- custom colorscheme, gitsigns and github.theme
+require("load_plugins") -- dashboard and orgmode configurations
+require("autocmd") -- custom auto commands

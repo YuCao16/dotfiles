9e6dd6708c0c369a8cb916042e56f65ef9fd11c0
YuCao16 <62466929+YuCao16@users.noreply.github.com>
2022-08-08 20:25:48 +0100

    add more exclude filetype for lens

--- dot_config/nvim/lua/vimplugin_setting.lua
+++ dot_config/nvim/lua/vimplugin_setting.lua
@@ -19,11 +19,22 @@ g["coc_snippet_next"] = "<tab>"
 -- "" tagbar settings
 -- "=====================================================
 g["tagbar_width"] = 34
+
+-- "=====================================================
+-- "" Lens/Animate settings
+-- "=====================================================
 g["lens#animate"] = 0
 g["animate#duration"] = 100.0
-g["lens#disabled_filetypes"] = { "tagbar", "nvimtree" }
--- cmd("let g:lens#disabled_filetypes = ['nerdtree','tagbar']")
--- cmd([[let g:tagbar_width = 50]])
+g["lens#disabled_filetypes"] = {
+	"tagbar",
+	"nvimtree",
+	"dapui_watches",
+	"dapui_stacks",
+	"dapui_breakpoints",
+	"dapui_scopes",
+	"dapui_console",
+	"dap-repl",
+}
 
 -- "=====================================================
 -- "" CSV settings
@@ -73,8 +84,8 @@ g["vim_markdown_conceal_code_blocks"] = 0
 -- "" Tex-Conceal.vim settings
 -- "=====================================================
 cmd([[set conceallevel=2]])
-g['tex_superscripts'] = "[0-9a-zA-W.,:;+-<>/()=]"
-g['tex_subscripts'] = "[0-9aehijklmnoprstuvx,+-/().]"
+g["tex_superscripts"] = "[0-9a-zA-W.,:;+-<>/()=]"
+g["tex_subscripts"] = "[0-9aehijklmnoprstuvx,+-/().]"
 g["tex_conceal"] = "abdmg"
 g["tex_flavor"] = "latex"
 -- cmd([[set concealcursor=nc]])
@@ -83,13 +94,13 @@ g["tex_flavor"] = "latex"
 -- "=====================================================
 -- "" everforest settings
 -- "=====================================================
-g['everforest_background'] = "hard"
-g['gruvbox_materiala_background'] = "hard"
+g["everforest_background"] = "hard"
+g["gruvbox_materiala_background"] = "hard"
 
 -- "=====================================================
 -- "" registers.nvim settings
 -- "=====================================================
-g['registers_delay'] = 200
-g['registers_window_border'] = "rounded"
-g['system_clip'] = 1
+g["registers_delay"] = 200
+g["registers_window_border"] = "rounded"
+g["system_clip"] = 1
 g["peekup_open"] = '"'

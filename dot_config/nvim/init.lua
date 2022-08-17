1796acd5c7f57092346d0f2dac272619542d64a6
YuCao16 <yu.carl@outlook.com>
2022-08-17 14:44:30 +0100

    adjust init.lua load order

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,15 +8,14 @@
 --++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
 vim.cmd([[let g:python3_host_prog="/usr/bin/python3"]])
 
-
 require("impatient") --speed up start up
-require("plugins") -- all plugins and some configs using packer.nvim
-require("bubbles") -- custom lualine
-require("mappings") -- custom mappings
-require("vimplugin_setting") -- vim plugins settings
 require("general_settings") -- neovim general settings
+require("plugins") -- all plugins and some configs using packer.nvim
 require("colorscheme") -- custom colorscheme, gitsigns and github.theme
+require("bubbles") -- custom lualine
 require("luaplugin_setting") -- dashboard and orgmode configurations
+require("vimplugin_setting") -- vim plugins settings
+require("mappings") -- custom mappings
 require("autocmd") -- custom auto commands
 require("functions") -- custom functions
 require("coc") -- coc.nvim configurations and keybindings

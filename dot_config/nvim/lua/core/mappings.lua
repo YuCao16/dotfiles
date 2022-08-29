9f83d9cf52bd7e98fac1e2130fcb76b7b9f79d84
YuCao16 <yu.carl@outlook.com>
2022-08-29 22:36:54 +0100

    some cleanup

--- dot_config/nvim/lua/core/mappings.lua
+++ dot_config/nvim/lua/core/mappings.lua
@@ -11,8 +11,8 @@ local g = vim.g -- Global variables
 local cmd = vim.cmd -- Execute Vim commands
 local map = vim.api.nvim_set_keymap
 g.mapleader = ","
-map("n", "<leader><leader>", ":noh<CR>", { noremap = false, silent = true })
-map("n", "<leader>p", ":TagbarToggle<CR>", { noremap = false, silent = false })
+
+-- Telescope
 map("n", "<leader>sf", ":Telescope find_files<CR>", { noremap = false, silent = false })
 map("n", "<leader>ss", ":Telescope find_files<CR>", { noremap = false, silent = false })
 map("n", "<leader>sg", ":Telescope live_grep<CR>", { noremap = false, silent = false })
@@ -20,20 +20,18 @@ map("n", "<leader>sb", ":Telescope buffers<CR>", { noremap = false, silent = fal
 map("n", "<leader>sh", ":Telescope help_tags<CR>", { noremap = false, silent = false })
 map("n", "<leader>so", ":Telescope oldfiles<CR>", { noremap = false, silent = false })
 map("n", "<leader>sl", ":SessionManager load_last_session<CR>", { noremap = false, silent = false })
+
+-- Nvimtree
 map("n", "<leader>q", ":lua Handle_nvimtree()<CR>", { noremap = false, silent = true })
 map("n", "<leader>Q", ":NvimTreeClose<CR>", { noremap = false, silent = true })
-map("n", "<leader>a", ":SidebarNvimToggle<CR>", { noremap = false, silent = false })
-map("n", "<leader>g", ":ZenMode<CR>", { noremap = true, silent = false })
-map("n", "<leader>l", ":Limelight!!<CR>", { noremap = false, silent = false })
-map("n", "<leader>b", ":BufferLineCycleNext<CR>", { noremap = false, silent = true })
-map("n", "<leader>B", ":BufferLineCyclePrev<CR>", { noremap = false, silent = true })
-map("n", "<leader>hp", ":Gitsigns preview_hunk<CR>", { noremap = false, silent = false })
-map("n", "<leader>w", "<C-w>", { noremap = false, silent = false })
-map("n", "<leader>f", ":Neoformat<cr>", { noremap = true, silent = false })
+
+-- ToggleTerm
 map("n", "<leader>cf", ":ToggleTerm direction=float<CR>", { noremap = false, silent = false })
 map("n", "<leader>cv", ":ToggleTerm size=40 direction=vertical<CR>", { noremap = false, silent = false })
 map("n", "<leader>ch", ":ToggleTerm direction=horizontal<CR>", { noremap = false, silent = false })
 map("n", "<leader>ct", ":ToggleTerm direction=tab<CR>", { noremap = false, silent = false })
+
+-- Dap
 map("n", "<leader>dc", ":lua require'dap'.continue()<CR>", { noremap = false, silent = false })
 map("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = false, silent = false })
 map("n", "<leader>ds", ":lua require'dap'.step_over()<CR>", { noremap = false, silent = false })
@@ -44,31 +42,54 @@ map(
 	{ noremap = false, silent = false }
 )
 map("n", "<leader>do", ":lua require'dapui'.toggle()<CR>", { noremap = false, silent = false })
--- map("n", "<leader><tab>", ":lua require('nvim-window').pick()<CR>", { noremap = false, silent = true })
+
+-- Windows resizer
+map("n", "<S-L>", ":exe 'vertical resize +5'<CR>", { noremap = true, silent = true })
+map("n", "<S-H>", ":exe 'vertical resize -5'<CR>", { noremap = true, silent = true })
+map("n", "<S-K>", ":exe 'resize -5'<CR>", { noremap = true, silent = true })
+map("n", "<S-J>", ":exe 'resize +5'<CR>", { noremap = true, silent = true })
+
+-- Bufferline
+map("n", "<leader>b", ":BufferLineCycleNext<CR>", { noremap = false, silent = true })
+map("n", "<leader>B", ":BufferLineCyclePrev<CR>", { noremap = false, silent = true })
+
+-- Utils
+map("n", "<leader>w", "<C-w>", { noremap = false, silent = false })
+map("t", "<ESC>", [[<C-\><C-n>]], { silent = true })
+map("n", "<leader><leader>", ":noh<CR>", { noremap = false, silent = true })
+
+-- Ohters
+map("n", "<leader>p", ":TagbarToggle<CR>", { noremap = false, silent = false })
+map("n", "<leader>tr", ":TroubleToggle<CR>", { noremap = false, silent = false })
+map("n", "<leader>ca", ":CodeActionMenu<CR>", { noremap = false, silent = false })
+map("n", "<leader>a", ":SidebarNvimToggle<CR>", { noremap = false, silent = false })
+map("n", "<leader>z", ":ZenMode<CR>", { noremap = true, silent = false })
+map("n", "<leader>hp", ":Gitsigns preview_hunk<CR>", { noremap = false, silent = false })
+map("n", "<leader>f", ":Neoformat<cr>", { noremap = true, silent = false })
+map("n", "<leader>0", "", {
+	callback = function()
+		ok, _ = pcall(vim.cmd, [[:lua require('nabla').popup({ border = 'single' })]])
+		if not ok then
+			vim.error("Put cursor inside latex expression")
+		end
+	end,
+	noremap = true,
+	silent = true,
+})
 map("n", "<leader><tab>", "", {
 	callback = function()
 		pcall(vim.cmd, [[:lua require('nvim-window').pick()]])
-		-- local ok, err = pcall(vim.cmd, t([[:lua require('nvim-window').pick()]]))
-		-- if not ok then
-		-- 	vim.cmd([[:lua require('nvim-window').pick()]])
-		-- else
-		-- 	vim.api.nvim_err_writeln("nvim-window has not been loaded!")
-		-- end
 	end,
 	noremap = false,
 	silent = true,
 })
 
-map("n", "<leader>0", ":lua require('nabla').popup({ border = 'single' })<CR>", { noremap = true, silent = false })
-map("n", "ga", "<Plug>(EasyAlign)", { noremap = false })
-map("n", "<S-L>", ":exe 'vertical resize +5'<CR>", { noremap = true, silent = true })
-map("n", "<S-H>", ":exe 'vertical resize -5'<CR>", { noremap = true, silent = true })
-map("n", "<S-K>", ":exe 'resize -5'<CR>", { noremap = true, silent = true })
-map("n", "<S-J>", ":exe 'resize +5'<CR>", { noremap = true, silent = true })
-map("x", "<leader>l", ":Limelight!!<CR>", { noremap = false, silent = false })
-map("x", "ga", "<Plug>(EasyAlign)", {})
-map("t", "<ESC>", [[<C-\><C-n>]], { silent = true })
-
+-- map("n", "<leader>0", ":lua require('nabla').popup({ border = 'single' })<CR>", { noremap = true, silent = false })
+-- map("n", "<leader>l", ":Limelight!!<CR>", { noremap = false, silent = false })
+-- map("n", "ga", "<Plug>(EasyAlign)", { noremap = false })
+-- map("x", "ga", "<Plug>(EasyAlign)", {})
+-- map("n", "<leader><tab>", ":lua require('nvim-window').pick()<CR>", { noremap = false, silent = true })
+-- map("x", "<leader>l", ":Limelight!!<CR>", { noremap = false, silent = false })
 -- map("n", "<leader>t", "gt<CR>", { noremap = false, silent = false })
 -- map("n", "<leader>ca", ":call CocActionAsync('codeAction','cursor')<cr>", { noremap = false, silent = false })
 -- cmd([[inoremap <silent><nowait><expr> <down> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<down>"]])
@@ -83,3 +104,15 @@ map("t", "<ESC>", [[<C-\><C-n>]], { silent = true })
 -- map("n", "<S-L>", ":call animate#window_delta_width(5)<CR>", { noremap = true })
 -- map("n", "<TAB>", ":call Show_documentation()<CR>", { noremap = ture, silent = false })
 -- map("n", "<leader>f", ":call SwitchFormatter()<CR>", { noremap = true, silent = false })
+-- map("n", "<leader><tab>", "", {
+-- 	callback = function()
+-- 		local ok, err = pcall(vim.cmd, t([[:lua require('nvim-window').pick()]]))
+-- 		if not ok then
+-- 			vim.cmd([[:lua require('nvim-window').pick()]])
+-- 		else
+-- 			vim.api.nvim_err_writeln("nvim-window has not been loaded!")
+-- 		end
+-- 	end,
+-- 	noremap = false,
+-- 	silent = true,
+-- })

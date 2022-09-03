ac142fe9d4891e40f572d8ef2edf24324f54759e
YuCao16 <yu.carl@outlook.com>
2022-09-03 20:42:49 +0100

    disable indent line show current context start

--- dot_config/nvim/lua/configs/indent-line.lua
+++ dot_config/nvim/lua/configs/indent-line.lua
@@ -11,7 +11,7 @@ vim.opt.listchars:append("eol:↴")
 require("indent_blankline").setup({
 	space_char_blankline = " ",
 	show_current_context = true,
-	show_current_context_start = true,
+	-- show_current_context_start = true,
 	filetype_exclude = {
 		"dashboard",
 		"lsp-installer",

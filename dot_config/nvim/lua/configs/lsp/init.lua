95686e57d18e2d98daa3a074b556b9992364b4de
YuCao16 <yu.carl@outlook.com>
2022-09-04 00:01:16 +0100

    lazy load cmp modify

--- dot_config/nvim/lua/configs/lsp/init.lua
+++ dot_config/nvim/lua/configs/lsp/init.lua
@@ -1,9 +1,9 @@
 local oks = {
-	["cmp"] = pcall(require, "configs.lsp.cmp"),
 	["lsp handlers"] = pcall(require, "configs.lsp.handlers"),
 	["lspsaga"] = pcall(require, "configs.lsp.lsp_saga"),
 	["null-ls"] = pcall(require, "configs.lsp.null"),
 	["signature"] = pcall(require, "configs.lsp.signature"),
+	-- ["cmp"] = pcall(require, "configs.lsp.cmp"), -- move to plugins to speed up startup
 }
 
 for name, item in pairs(oks) do

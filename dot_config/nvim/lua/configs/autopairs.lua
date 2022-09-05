3795447d7259565a26304016205ac8f84e7d9afb
YuCao16 <yu.carl@outlook.com>
2022-09-06 00:34:45 +0100

    code cleanup

--- dot_config/nvim/lua/configs/autopairs.lua
+++ dot_config/nvim/lua/configs/autopairs.lua
@@ -1,3 +1,37 @@
+local apairs_ok, apairs = pcall(require, "nvim-autopairs")
+if not apairs_ok then
+	vim.notify("nvim-autopairs failed", "error", { render = "minimal" })
+	return
+end
+
+apairs.setup({
+	check_ts = true,
+	ts_config = {
+		lua = { "string", "source" },
+		javascript = { "string", "template_string" },
+		java = false,
+	},
+	disable_filetype = { "TelescopePrompt", "spectre_panel" },
+	fast_wrap = {
+		map = "<M-e>",
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+		offset = 0,
+		end_key = "$",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		highlight = "PmenuSel",
+		highlight_grey = "LineNr",
+	},
+})
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_ok, cmp = pcall(require, "cmp")
+if not cmp_ok then
+	return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { text = "" } }))
+
 -- local remap = vim.api.nvim_set_keymap
 -- local npairs = require("nvim-autopairs")
 -- npairs.setup({ map_cr = false, disabled_filetypes = { "latex", "tex" } })
@@ -13,36 +47,3 @@
 -- 	end
 -- end
 -- remap("i", "<CR>", "v:lua.MUtils.completion_confirm()", { expr = true, noremap = true })
-local ok, apairs = pcall(require, 'nvim-autopairs')
-if not ok then
-    return
-end
-
-apairs.setup{
-    check_ts = true,
-    ts_config = {
-        lua = {'string', 'source'},
-        javascript = {'string', 'template_string'},
-        java = false,
-    },
-    disable_filetype = {'TelescopePrompt', 'spectre_panel'},
-    fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'", },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0,
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-    },
-}
-
-local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-local cmp_ok, cmp = pcall(require, 'cmp')
-if not cmp_ok then
-    return
-end
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done {map_char = { text = ""}})
-

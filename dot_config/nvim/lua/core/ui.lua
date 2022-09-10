5067eb4341c2bacce5266208cb1b4c69b1fffeff
YuCao16 <yu.carl@outlook.com>
2022-09-10 17:16:24 +0100

    icons for cmp kind

---
+++ dot_config/nvim/lua/core/ui.lua
@@ -0,0 +1,71 @@
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+--            ███╗░░██╗███████╗░█████╗░██╗░░░██╗██╗███╗░░░███╗		  --
+--            ████╗░██║██╔════╝██╔══██╗██║░░░██║██║████╗░████║		  --
+--            ██╔██╗██║█████╗░░██║░░██║╚██╗░██╔╝██║██╔████╔██║		  --
+--            ██║╚████║██╔══╝░░██║░░██║░╚████╔╝░██║██║╚██╔╝██║		  --
+--            ██║░╚███║███████╗╚█████╔╝░░╚██╔╝░░██║██║░╚═╝░██║		  --
+--            ╚═╝░░╚══╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝		  --
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+
+local M = {}
+-------------------
+-- kind icon for cmp
+-------------------
+M.kind_icons = {
+	Text = "",
+	Method = "m",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+}
+
+M.unicons = {
+	File = " ",
+	Module = " ",
+	Namespace = " ",
+	Package = " ",
+	Class = " ",
+	Method = " ",
+	Property = " ",
+	Field = " ",
+	Constructor = " ",
+	Enum = "練",
+	Interface = "練",
+	Function = " ",
+	Variable = " ",
+	Constant = " ",
+	String = " ",
+	Number = " ",
+	Boolean = "◩ ",
+	Array = " ",
+	Object = " ",
+	Key = " ",
+	Null = "ﳠ ",
+	EnumMember = " ",
+	Struct = " ",
+	Event = " ",
+	Operator = " ",
+	TypeParameter = " ",
+}
+
+return M

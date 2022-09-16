b24bb685ebd2ccef442d8fc6c7b5e5651e578907
YuCao16 <yu.carl@outlook.com>
2022-09-16 22:13:14 +0100

    modify sumneko_lua config for lua-dev

--- dot_config/nvim/lua/configs/lsp/settings/sumneko_lua.lua
+++ dot_config/nvim/lua/configs/lsp/settings/sumneko_lua.lua
@@ -6,9 +6,14 @@ table.insert(runtime_path, "lua/?/init.lua")
 return {
 	settings = {
 		Lua = {
+			hint = {
+				enable = true,
+				setType = true,
+			},
 			runtime = {
 				-- Tell the language server which version of Lua you're using
 				-- (most likely LuaJIT in the case of Neovim)
+				-- path = { "lua/?.lua", "lua/?/init.lua" },
 				version = "LuaJIT",
 				-- path = runtime_path,
 			},
@@ -27,15 +32,19 @@ return {
 			workspace = {
 				-- Make the server aware of Neovim runtime files
 				-- library = vim.api.nvim_get_runtime_file("", true),
+				-- library = {
+				-- 	[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+				-- 	[vim.fn.stdpath("config") .. "/lua"] = true,
+				-- },
 				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
+					"/home/caoyu/.local/share/nvim/site/pack/packer/start/lua-dev.nvim/types",
+					"/usr/share/nvim/runtime",
 				},
 				maxPreload = 2000,
 				preloadFileSize = 50000,
 			},
-			completion = { callSnippet = "Both" },
-			-- completion = { callSnippet = "Replace" },
+			-- completion = { callSnippet = "Both" },
+			completion = { callSnippet = "Replace" },
 			telemetry = { enable = false },
 		},
 	},

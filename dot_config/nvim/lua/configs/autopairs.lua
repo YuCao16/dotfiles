a683d5ffbd2b7cbc2527b895aeb342cf57db0d79
YuCao16 <yu.carl@outlook.com>
2022-09-16 22:14:24 +0100

    add autopairs rules for tex

--- dot_config/nvim/lua/configs/autopairs.lua
+++ dot_config/nvim/lua/configs/autopairs.lua
@@ -25,6 +25,26 @@ apairs.setup({
 	},
 })
 
+local cond = require("nvim-autopairs.conds")
+local Rule = require("nvim-autopairs.rule")
+apairs.add_rules(
+	{
+		Rule("$", "$", { "tex", "latex" })
+			-- don't add a pair if the next character is %
+			:with_pair(cond.not_after_regex("%%"))
+			-- don't add a pair if  the previous character is xxx
+			:with_pair(cond.not_before_regex("xxx", 3))
+			-- don't move right when repeat character
+			:with_move(cond.none())
+			-- don't delete if the next character is xx
+			:with_del(cond.not_after_regex("xx"))
+			-- disable adding a newline when you press <cr>
+			:with_cr(cond.none()),
+	},
+	-- disable for .vim files, but it work for another filetypes
+	Rule("a", "a", "-vim")
+)
+
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_ok, cmp = pcall(require, "cmp")
 if not cmp_ok then
@@ -47,3 +67,13 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { text =
 -- 	end
 -- end
 -- remap("i", "<CR>", "v:lua.MUtils.completion_confirm()", { expr = true, noremap = true })
+
+-- apairs.add_rules({
+-- 	Rule("$$", "$$", "tex"):with_pair(function(opts)
+-- 		print(vim.inspect(opts))
+-- 		if opts.line == "aa $$" then
+-- 			-- don't add pair on that line
+-- 			return false
+-- 		end
+-- 	end),
+-- })

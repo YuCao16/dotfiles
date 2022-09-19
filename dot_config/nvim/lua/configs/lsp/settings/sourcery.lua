9e60723667563133da0cd05350f07067a3588748
YuCao16 <yu.carl@outlook.com>
2022-09-19 23:48:57 +0100

    find token safely

--- dot_config/nvim/lua/configs/lsp/settings/sourcery.lua
+++ dot_config/nvim/lua/configs/lsp/settings/sourcery.lua
@@ -1,15 +1,20 @@
 local function readAll(file)
-	local f = assert(io.open(file, "rb"))
-	local content = f:read("*all")
-	f:close()
-	return content
+    local f = assert(io.open(file, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
 end
 
-local token = readAll("/home/caoyu/.config/sourcery/tokenizer.txt")
-token = string.gsub(token, "\n", "")
+-- local token = readAll("/home/caoyu/.config/sourcery/tokenizer.txt")
+local token_ok, token =
+    pcall(readAll, "/home/caoyu/.config/sourcery/tokenizer.txt")
+if not token_ok then
+    return
+end
 
+token = string.gsub(token, "\n", "")
 return {
-	init_options = {
-		token = token,
-	},
+    init_options = {
+        token = token,
+    },
 }

530bb1051be0a4525d423f59ec695399c035ca2a
YuCao16 <yu.carl@outlook.com>
2022-09-19 23:03:01 +0100

    remove comments

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -31,15 +31,6 @@ local oks = {
 	["bufferline"] = pcall(require, "configs.bufferline"),
 	["treesitter"] = pcall(require, "configs.treesitter"),
 	["telescope"] = pcall(require, "configs.telescope"),
-
-	-- Move to plugin for lazy loading
-	-- ["gitsigns"] = pcall(require, "configs.gitsign"),
-	-- ["lsp"] = pcall(require, "configs.lsp"),
-	-- ["Dap"] = pcall(require, "configs.dap"),
-
-	-- currenyly not using
-	-- ["coc"] = pcall(require, "configs.coc"),
-	-- ["neotree"] = pcall(require, "configs.neotree"),
 }
 
 notif_ok, vim.notify = pcall(require, "notify")
@@ -51,21 +42,9 @@ require("notify").setup({
 })
 
 for name, item in pairs(oks) do
+	---@diagnostic disable-next-line: unbalanced-assignments
 	local ok, _ = item
 	if not ok and notif_ok then
 		vim.notify(name .. " failed", "error", { title = name .. ".lua", render = "minimal" })
 	end
 end
-
--- require("impatient") --speed up start up
--- require("general_settings") -- neovim general settings
--- require("plugins") -- all plugins and some configs using packer.nvim
--- require("colorscheme") -- custom colorscheme, gitsigns and github.theme
--- require("bubbles") -- custom lualine
--- require("luaplugin_setting") -- dashboard and orgmode configurations
--- require("vimplugin_setting") -- vim plugins settings
--- require("mappings") -- custom mappings
--- require("autocmd") -- custom auto commands
--- require("functions") -- custom functions
--- require("coc") -- coc.nvim configurations and keybindings
--- vim.cmd("source $HOME/.config/nvim/settings.vim")

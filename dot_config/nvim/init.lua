7b83c9d9f40f038816a97dc2a363118eb19b2b60
YuCao16 <yu.carl@outlook.com>
2022-09-23 23:27:31 +0100

    format

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,42 +9,46 @@
 
 local impatient_ok, impatient = pcall(require, "impatient")
 if impatient_ok then
-	impatient.enable_profile()
+    impatient.enable_profile()
 end
 
 local oks = {
 
-	-- core
-	["options"] = pcall(require, "core.options"),
-	["plugins"] = pcall(require, "core.plugins"),
-	["colorscheme"] = pcall(require, "core.colorscheme"),
-	["highlight"] = pcall(require, "core.highlight"),
-	["mappings"] = pcall(require, "core.mappings"),
-	["autocmd"] = pcall(require, "core.autocmd"),
-	["functions"] = pcall(require, "core.functions"),
+    -- core
+    ["options"] = pcall(require, "core.options"),
+    ["plugins"] = pcall(require, "core.plugins"),
+    ["colorscheme"] = pcall(require, "core.colorscheme"),
+    ["highlight"] = pcall(require, "core.highlight"),
+    ["mappings"] = pcall(require, "core.mappings"),
+    ["autocmd"] = pcall(require, "core.autocmd"),
+    ["functions"] = pcall(require, "core.functions"),
 
-	-- configs
-	["vimplugin_setting"] = pcall(require, "configs.vimplugin_configs"),
-	["dashboard"] = pcall(require, "configs.dashboard"),
-	["nvimtree"] = pcall(require, "configs.nvimtree"),
-	["lualine"] = pcall(require, "configs.lualine"),
-	["bufferline"] = pcall(require, "configs.bufferline"),
-	["treesitter"] = pcall(require, "configs.treesitter"),
-	["telescope"] = pcall(require, "configs.telescope"),
+    -- configs
+    ["vimplugin_setting"] = pcall(require, "configs.vimplugin_configs"),
+    ["dashboard"] = pcall(require, "configs.dashboard"),
+    ["nvimtree"] = pcall(require, "configs.nvimtree"),
+    ["lualine"] = pcall(require, "configs.lualine"),
+    ["bufferline"] = pcall(require, "configs.bufferline"),
+    ["treesitter"] = pcall(require, "configs.treesitter"),
+    ["telescope"] = pcall(require, "configs.telescope"),
 }
 
 notif_ok, vim.notify = pcall(require, "notify")
 require("notify").setup({
-	on_open = function(win)
-		-- set vim.notify() not focusable
-		-- vim.api.nvim_win_set_config(win, { focusable = false })
-	end,
+    on_open = function(win)
+        -- set vim.notify() not focusable
+        -- vim.api.nvim_win_set_config(win, { focusable = false })
+    end,
 })
 
 for name, item in pairs(oks) do
-	---@diagnostic disable-next-line: unbalanced-assignments
-	local ok, _ = item
-	if not ok and notif_ok then
-		vim.notify(name .. " failed", "error", { title = name .. ".lua", render = "minimal" })
-	end
+    ---@diagnostic disable-next-line: unbalanced-assignments
+    local ok, _ = item
+    if not ok and notif_ok then
+        vim.notify(
+            name .. " failed",
+            "error",
+            { title = name .. ".lua", render = "minimal" }
+        )
+    end
 end

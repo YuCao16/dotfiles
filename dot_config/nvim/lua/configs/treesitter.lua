68e59554fd39df9316e6518dfb4ef9cdc3225549
YuCao16 <yu.carl@outlook.com>
2022-12-05 18:47:50 +0000

    remove useless code

--- dot_config/nvim/lua/configs/treesitter.lua
+++ dot_config/nvim/lua/configs/treesitter.lua
@@ -53,40 +53,41 @@ require("nvim-treesitter.configs").setup({
     },
 })
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
 --=====================================================
 -- Orgmode parsers
 --=====================================================
-parser_config.org = {
-    install_info = {
-        url = "https://github.com/milisims/tree-sitter-org",
-        revision = "main",
-        files = { "src/parser.c", "src/scanner.cc" },
-    },
-    filetype = "org",
-}
+-- parser_config.org = {
+--     install_info = {
+--         url = "https://github.com/milisims/tree-sitter-org",
+--         revision = "main",
+--         files = { "src/parser.c", "src/scanner.cc" },
+--     },
+--     filetype = "org",
+-- }
 
 --=====================================================
 -- Neorg parsers
 --=====================================================
-parser_config.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
-parser_config.norg = {
-    install_info = {
-        url = "https://github.com/vhyrro/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main",
-    },
-}
-parser_config.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
+-- parser_config.norg_meta = {
+--     install_info = {
+--         url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+--         files = { "src/parser.c" },
+--         branch = "main",
+--     },
+-- }
+-- parser_config.norg = {
+--     install_info = {
+--         url = "https://github.com/vhyrro/tree-sitter-norg",
+--         files = { "src/parser.c", "src/scanner.cc" },
+--         branch = "main",
+--     },
+-- }
+-- parser_config.norg_table = {
+--     install_info = {
+--         url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+--         files = { "src/parser.c" },
+--         branch = "main",
+--     },
+-- }

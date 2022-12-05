5a8d633546ca5cf38187be32a76836acda60896e
YuCao16 <yu.carl@outlook.com>
2022-12-05 18:45:39 +0000

    add ruff config

--- dot_config/nvim/lua/configs/lsp/null.lua
+++ dot_config/nvim/lua/configs/lsp/null.lua
@@ -1,4 +1,6 @@
 local null_ls = require("null-ls")
+local methods = require("null-ls.methods")
+local helpers = require("null-ls.helpers")
 
 local is_executable = function(cmd_name, cond)
     local u = require("null-ls.utils")
@@ -11,34 +13,98 @@ local is_executable = function(cmd_name, cond)
     end
 end
 
+local function ruff_fix()
+    return helpers.make_builtin({
+        name = "ruff",
+        meta = {
+            url = "https://github.com/charliermarsh/ruff/",
+            description = "An extremely fast Python linter, written in Rust.",
+        },
+        method = methods.internal.FORMATTING,
+        filetypes = { "python" },
+        generator_opts = {
+            command = "ruff",
+            args = {
+                "--fix",
+                "-e",
+                "-n",
+                "--stdin-filename",
+                "$FILENAME",
+                "--unfixable F401",
+                -- "--unfixable I001",
+                "-",
+            },
+            to_stdin = true,
+        },
+        factory = helpers.formatter_factory,
+    })
+end
+
 null_ls.setup({
+    border = "rounded",
     sources = {
-        -- it says buildins, but you still need to use Mason to install
+        -- Markdown
         null_ls.builtins.diagnostics.markdownlint.with({
             extra_args = { "--disable=line_length" },
             condition = is_executable("markdownlint"),
         }),
-        -- null_ls.builtins.diagnostics.cpplint,
-        -- null_ls.builtins.diagnostics.mypy.with({
-        -- 	extra_args = { "--ignore-missing-imports" },
-        -- 	condition = is_executable("mypy"),
+
+        -- Python
+        null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length=80" },
+            condition = is_executable("black"),
+        }),
+
+        ruff_fix(),
+        null_ls.builtins.diagnostics.ruff.with({
+            extra_args = {
+                "--ignore=E501",
+                "--ignore=E402",
+                "--ignore=ANN101",
+                "--ignore=ANN401",
+                "--extend-select=I",
+                "--extend-select=ANN",
+                "--ignore=F405",
+                "--ignore=F401",
+                "--ignore=I001",
+                -- "--extend-ignore=ANN",
+            },
+            condition = is_executable("ruff"),
+        }),
+        -- null_ls.builtins.formatting.isort.with({
+        --     extra_args = { "--profile=black" },
+        --     condition = is_executable("isort"),
         -- }),
-        -- null_ls.builtins.formatting.eslint,
-        -- null_ls.builtins.formatting.cmakelang,
-        -- null_ls.builtins.formatting.clang_format,
-        -- null_ls.builtins.diagnostics.pylint.with({
-        -- 	extra_args = { "--disable=missing-docstring", "--errors-only" },
-        -- 	condition = is_executable('pylint')
+        -- null_ls.builtins.diagnostics.mypy.with({
+        --     extra_args = {
+        --         "--ignore-missing-imports",
+        --         "--cache-dir=/Users/caoyu/mypycache/",
+        --         "--disallow-untyped-defs",
+        --         "--disallow-incomplete-defs",
+        --         "--check-untyped-defs",
+        --         "--strict",
+        --     },
+        --     condition = is_executable("mypy"),
         -- }),
         -- null_ls.builtins.diagnostics.pylama,
-        -- null_ls.builtins.diagnostics.vulture.with({
-        -- 	method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-        -- 	condition = is_executable('pylint')
+        -- null_ls.builtins.diagnostics.flake8.with({
+        --     extra_args = { "--ignore=E501" },
+        --     condition = is_executable("flake8"),
         -- }),
-        -- formatting.isort,
-        -- diag.write_good,
-        -- formatting.black.with({
-        --     extra_args = { "--fast" },
+        -- null_ls.builtins.diagnostics.pylint.with({
+        --     extra_args = { "--disable=missing-docstring", "--errors-only" },
+        --     condition = is_executable("pylint"),
         -- }),
+
+        -- Cpp & CMake
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.alex,
+        -- null_ls.builtins.diagnostics.cspell,
+
+        -- Cpp & CMake
+        -- null_ls.builtins.diagnostics.cpplint,
+        -- null_ls.builtins.formatting.cmakelang,
+        -- null_ls.builtins.formatting.clang_format,
     },
 })

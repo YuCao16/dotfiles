b36a227810a8ca27d9d492e473cf8d8da4e5e03f
YuCao16 <yu.carl@outlook.com>
2022-12-05 18:46:10 +0000

    enable current_line_blame

--- dot_config/nvim/lua/configs/gitsign.lua
+++ dot_config/nvim/lua/configs/gitsign.lua
@@ -6,12 +6,19 @@ end
 
 gitsigns.setup({
     signs = {
-        add = { hl = "GitSignsAdd", text = " +" },
-        change = { hl = "GitSignsChange", text = " ~" },
-        delete = { hl = "GitSignsDelete", text = " _" },
-        topdelete = { hl = "GitSignsDelete", text = " ‾" },
-        changedelete = { hl = "GitSignsChange", text = " ~" },
+        add = { hl = "GitSignsAdd", text = "+" },
+        change = { hl = "GitSignsChange", text = "~" },
+        delete = { hl = "GitSignsDelete", text = "_" },
+        topdelete = { hl = "GitSignsDelete", text = "‾" },
+        changedelete = { hl = "GitSignsChange", text = "~" },
     },
+    -- signs = {
+    --     add = { hl = "GitSignsAdd", text = " +" },
+    --     change = { hl = "GitSignsChange", text = " ~" },
+    --     delete = { hl = "GitSignsDelete", text = " _" },
+    --     topdelete = { hl = "GitSignsDelete", text = " ‾" },
+    --     changedelete = { hl = "GitSignsChange", text = " ~" },
+    -- },
     -- signs = {
     -- 	add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
     -- 	change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -28,13 +35,14 @@ gitsigns.setup({
         follow_files = true,
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
         ignore_whitespace = false,
     },
+    current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary>",
     current_line_blame_formatter_opts = {
         relative_time = false,
     },
